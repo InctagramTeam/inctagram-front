692ba613780906e70ce4725059ca496a0ceef697
openservicedv <150409269+openservicedv@users.noreply.github.com>
2024-05-09 13:59:22 -0700

    IN-56 feat: add card ui

--- src/components/Card/Cards.tsx
+++ src/components/Card/Cards.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
 import { Card } from '@/components/ui/card/card'
 
 export const Cards = () => {
   return (
-    <Card className="w-[100px] h-[100px] bg-[#171717] border border-[#333333] rounded-sm"></Card>
+    <Card className={'w-[100px] h-[100px] bg-[#171717] border border-[#333333] rounded-sm'}></Card>
   )
 }
 
