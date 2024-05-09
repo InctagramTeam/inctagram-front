b0d9ec3fc7a43ef9a618f0d97a2627f962e1481b
openservicedv <150409269+openservicedv@users.noreply.github.com>
2024-05-09 13:59:46 -0700

    IN-56 feat: add card ui

--- src/components/Card/Cards.tsx
+++ src/components/Card/Cards.tsx
@@ -5,7 +5,3 @@ export const Cards = () => {
     <Card className={'w-[100px] h-[100px] bg-[#171717] border border-[#333333] rounded-sm'}></Card>
   )
 }
-
-// background: #171717;
-// border: 1px solid #333333;
-// border-radius: 2px;
