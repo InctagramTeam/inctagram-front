f41ce2547f127ff9a85d4a99690816b20dba6695
openservicedv <150409269+openservicedv@users.noreply.github.com>
2024-05-09 13:57:01 -0700

    IN-56 init: add card ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import { getLayout } from '@/components/Layout/Layout'
 import Link from 'next/link'
 import instagram from 'public/inctagram.png'
 import { Page } from '@/components/ui/page'
-import { Card } from '@/components/ui/card/card'
 import { Cards } from '@/components/Card/Cards'
 
 function Public() {
@@ -18,9 +17,6 @@ function Public() {
         <Link href={'/search'}>Search</Link>
         <Link href={'/favorites'}>Favorites</Link>
       </nav>
-      <div>
-        <Cards />
-      </div>
     </Page>
   )
 }
