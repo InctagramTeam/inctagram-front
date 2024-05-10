84240153fb8f1dd29ebca3767357b585e053964c
openservicedv <150409269+openservicedv@users.noreply.github.com>
2024-05-10 15:23:37 -0700

    IN-50 init: add recaptcha ui

--- src/components/ui/recaptcha/recaptcha.tsx
+++ src/components/ui/recaptcha/recaptcha.tsx
@@ -1,9 +1,53 @@
 import React from 'react'
 import { Recaptchalogo1 } from '@/assets/icons'
+import CheckIcon from '@/assets/icons/CheckIcon'
+import CheckDisabledIcon from '@/assets/icons/CheckDisabledIcon'
 
-const Box = () => {
-  return <div className={'w-[20px] h-[20px] border rounded-sm border-[#B7B7B7] bg-[#FFFFFF]'}></div>
+const BoxDefault = () => {
+  return (
+    <div className={'w-[20px] h-[20px] border rounded-[1px] border-[#B7B7B7] bg-[#FFFFFF]'}></div>
+  )
 }
+const BoxChecked = () => {
+  return (
+    <div className={'flex justify-center items-center w-[24px] h-[18px]'}>
+      <svg
+        width="25"
+        height="19"
+        viewBox="0 0 25 19"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M22.0355 0.749512C22.8113 1.49441 23.5691 2.25975 24.3387 3.01177V3.01272C19.0046 8.34204 13.671 13.6728 8.3341 19.0002C5.56224 16.2222 2.78041 13.436 0.0147363 10.6642L0 10.6656V10.6509C0.0656004 10.6651 0.0646496 10.5411 0.126922 10.5221C0.841871 9.80664 1.55159 9.08551 2.26701 8.37009C4.29112 10.3904 6.31237 12.414 8.33457 14.4362C12.9033 9.87557 17.4716 5.31492 22.0355 0.749512Z"
+          fill="#19983B"
+          fillOpacity="0.901702"
+        />
+      </svg>
+    </div>
+  )
+}
+const BoxLoading = () => {
+  return (
+    <div className={'flex justify-center items-center w-[13px] h-[13px]'}>
+      <svg
+        width="18"
+        height="17"
+        viewBox="0 0 18 17"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M2.5 2C6.69126 2.20312 15.159 5.0875 15.5 15"
+          stroke="#4D8DF4"
+          stroke-width="4"
+          stroke-linecap="round"
+        />
+      </svg>
+    </div>
+  )
+}
+
 const Privacy = () => {
   return (
     <div className={'flex justify-center items-center w-[42px] h-[7px] text-[6px] font-medium'}>
@@ -11,16 +55,41 @@ const Privacy = () => {
     </div>
   )
 }
+const Expired = () => {
+  return (
+    <div
+      className={'absolute left-4 top-0.5 w-[175px] h-[24px] text-[10px] text-[#FF0000] leading-3'}
+    >
+      Verification expired. Check the checkbox again.
+    </div>
+  )
+}
 
+// position: absolute;
+// width: 175px;
+// height: 24px;
+// left: 17px;
+// top: 4px;
+//
+// font-family: 'Roboto';
+// font-style: normal;
+// font-weight: 400;
+// font-size: 10px;
+// line-height: 12px;
+
+// color: #FF0000
 const Recaptcha = () => {
   return (
     <div
       className={
-        'flex justify-around items-center w-[300px] h-[79px] border rounded-sm border-[#333333] bg-[#171717]'
+        'relative flex justify-around items-center w-[300px] h-[79px] border rounded-sm border-[#333333] bg-[#171717]'
       }
     >
+      <Expired />
       <div className={'flex justify-between items-center w-[111px] h-[20px]'}>
-        <Box />
+        <BoxDefault />
+        {/*<BoxChecked />*/}
+        {/*<BoxLoading />*/}
         {/*<div className={'w-[20px] h-[20px] border rounded-sm border-[#B7B7B7] bg-[#FFFFFF]'}></div>*/}
         <div className={'text-xs font-medium text-[#FFFFFF]'}>I’m not a robot</div>
       </div>
