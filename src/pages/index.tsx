9396f8d659d5f9207c475610ebb4ef3c64cadba0
openservicedv <150409269+openservicedv@users.noreply.github.com>
2024-05-10 13:21:46 -0700

    IN-50 init: add recaptcha ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import { getLayout } from '@/components/Layout/Layout'
 import Link from 'next/link'
 import instagram from 'public/inctagram.png'
 import { Page } from '@/components/ui/page'
-import { Recaptchalogo1 } from '@/assets/icons'
 
 function Public() {
   return (
