a3a9989ab80952ef029c9535a9aaa378bed0d67f
Alex666000 <bogdanov777000@mail.ru>
2024-05-13 17:07:40 +0300

    fix Input

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -1,57 +1,111 @@
 import {
   ChangeEvent,
+  ComponentProps,
   ComponentPropsWithoutRef,
-  KeyboardEvent,
-  ReactNode,
   forwardRef,
+  useId,
+  useRef,
   useState,
 } from 'react'
+import { clsx } from 'clsx'
 
+import { Text } from '@/components/ui/text/text'
 import { CloseIcon, EyeIcon, EyeOffIcon, SearchIcon } from '@/assets/icons'
-import * as LabelPrimitive from '@radix-ui/react-label'
-import clsx from 'clsx'
+import { mergeRefs } from '@/utils/merge-refs'
 
-export type InputProps = {
-  classNameInput?: string
-  disabled?: boolean
-  /**
-   * Показ иконки внутри инпута: слева или справа от текста Инпута
-   */
-  endIcon?: ReactNode
-  error?: string
+export type TextFieldProps = {
+  // У нас компонент инпут, но он вложен в див и если нам понадобится, например, добавить марджины
+  // там где используется инпут,
+  // то надо будет присвоить ему класс и применить стили
+  // Но класс надо будет передать именно в контейнер, то есть див, а не в сам инпут,
+  // чтобы марджины были у инпута,
+  // лэйбла и у всего того что в контейнере)
+  // Пример:
+  // <FormTextField
+  // inputProps={{ className: s.formTextField }} // этот класс пойдет в инпут
+  // className = {'divClassName'} // этот пойдет в див
+  // /> -- То же самое с labelProps
+  containerProps?: ComponentProps<'div'>
+  errorMessage?: string
   label?: string
+  disabled?: boolean
+  labelProps?: ComponentProps<'label'>
+  /**
+   * Обратный вызов, который вызывается при нажатии на кнопку очистки
+   * Если не указан, очищает внутреннее значение через ref и вызывает onValueChange с пустой строкой
+   */
+  onClearInput?: () => void
   onValueChange?: (value: string) => void
-  startIcon?: ReactNode
-  type?: 'email' | 'password' | 'search' | 'text'
-} & Omit<ComponentPropsWithoutRef<'input'>, 'type'>
+  // Так как мы делаем компоненты универсальными и нам нужны все возможные пропсы,
+  // которые мы можем передать в нативный элемент,
+  // то есть html тег, то мы используем тип ComponentPropsWithoutRef<‘input’>
+  // и в дженерике указываем для какого именно тэга
+} & ComponentPropsWithoutRef<'input'>
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(
+export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
   (
     {
       className,
-      classNameInput,
-      disabled,
-      error = '',
+      containerProps,
+      errorMessage = 'Error',
       id,
-      label = '',
-      onKeyDown,
+      disabled,
+      label,
+      labelProps,
+      onChange,
+      onClearInput,
       onValueChange,
-      type = 'search',
-      value,
-      ...rest
+      placeholder,
+      type = 'text',
+      ...restProps
     },
-    ref
+    forwardedRef
   ) => {
-    const [isVisible, setIsVisible] = useState(false)
+    const generatedId = useId()
+    const finalId = id ?? generatedId
 
-    const classes = {
+    const inputRefOrTextareaRef = useRef<HTMLInputElement | HTMLTextAreaElement>(null)
+    const finalRef = mergeRefs([forwardedRef, inputRefOrTextareaRef])
+    const [revealPassword, setRevealPassword] = useState(false)
+
+    const isRevealPasswordButtonShown = type === 'password'
+    const isSearchField = type === 'search'
+    const isClearInputButtonShown = isSearchField
+
+    const finalType = getFinalType(type, revealPassword)
+
+    function handleChange(e: ChangeEvent<HTMLInputElement>) {
+      onChange?.(e)
+      onValueChange?.(e.target.value)
+    }
+
+    function handleToggleShowPassword() {
+      setRevealPassword((prevState: boolean) => !prevState)
+    }
+
+    function handleClearInput() {
+      if (onClearInput) {
+        return onClearInput()
+      }
+
+      if (!inputRefOrTextareaRef.current) {
+        return
+      }
+      inputRefOrTextareaRef.current.value = ''
+      onValueChange?.('')
+    }
+
+    const classNames = {
+      error: clsx(
+        errorMessage && `_Error_ text-small-text-12 text-Danger-500 mt-1 `,
+        containerProps?.className
+      ),
       input: clsx(
-        `Input relative w-full p-[6px_42px_6px_12px] text-regular-text-16 text-Light-900 bg-transparent border-none 
-        rounded-sm ring-1 px-[40px] shadow-sm shadow-Dark-300 ring-Dark-100 outline-1 outline-Dark-100
-        transition-all duration-150 easy-in-out
-        focus:outline-none
-        focus:ring-1 focus:ring-offset-1 focus:ring-opacity-50 focus:text-Light-900
-        focus:ring-offset-Primary-500 focus:opacity-60 focus:shadow-sm focus:shadow-Primary-500
+        `Input relative w-full h-full min-h-[36px] p-[6px_42px_6px_12px] text-regular-text-16 text-Light-900
+        bg-transparent border-none rounded-sm ring-1 px-[40px] shadow-sm shadow-Dark-300 ring-Dark-100 outline-1
+        outline-Dark-100
+        focus:outline-none focus:transition-all duration-150 easy-in-out focus:ring-1
+        focus:ring-offset-Primary-500 focus:opacity-60 focus:text-Light-100 focus:shadow-sm focus:shadow-Light-500
         focus-visible:outline-none focus-visible:ring-1 focus-visible:offset-1
         focus-visible:ring-opacity-50 focus-visible:ring-offset-Primary-500
         active:not:disabled:text-Light-100 active:not:disabled:outline
@@ -59,156 +113,108 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
         hover:not:disabled:outline hover:not:disabled:outline-1
         hover:not:disabled:outline-text-Light-900
         hover:not:focus-visible:outline-1 hover:not:focus-visible:outline-text-Light-900
-        file:border-0 file:bg-transparent file:font-inter disabled:cursor-not-allowed disabled:opacity-50
-        `,
+        file:border-0 file:bg-transparent file:font-inter disabled:cursor-not-allowed disabled:opacity-50`,
         disabled && `text-Dark-100`,
-        error && 'ring-2 ring-Danger-500',
         [type],
-        type === 'text' || (type === 'email' && `pr-3`),
-        {
-          [`text-Light-100 outline outline-Danger-500 hover:not:disabled-visible:outline
-          hover:not:disabled-visible:outline-1 hover:not:disabled-visible:outline-text-Light-900`]:
-            error,
-        },
-        classNameInput
+        type === 'text' || (type === 'email' && `pr-3`)
       ),
-      label: clsx(`text-medium-text-14 text-Light-900`, { [`text-Dark-100`]: disabled }),
-      textField: clsx(`flex flex-col`, className),
-    }
-
-    const onVisible = () => {
-      setIsVisible(prevState => !prevState)
-    }
-
-    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-      rest.onChange?.(e)
-      onValueChange?.(e.target.value)
-    }
-    const clearFieldHandler = () => {
-      onValueChange?.('')
-    }
-
-    const onKeydownHandler = (e: KeyboardEvent<HTMLInputElement>) => {
-      if (e.key === 'Enter') {
-        e.preventDefault()
-        onKeyDown && onKeyDown(e)
-      }
+      inputContainer: clsx(`_FieldContainer_ relative w-full text-regular-text-16 text-Light-900`),
+      label: clsx(
+        `_Label_ mb-[1px] text-Dark-100 text-regular-text-14 text-Light-900`,
+        { [`text-Dark-100`]: disabled },
+        labelProps?.className
+      ),
+      searchIcon: clsx(
+        `_LeadingIcon_ absolute text-Light-900 top-1/2 bottom-1/2 left-0 transform -translate-t-1/2
+      w-[20px] h-[20px] ml-[12px] p-0 bg-transparent border-o outline-0 ring-0
+      focus-visible:text-Light-100 focus-visible:outline focus-visible:outline-2
+      focus-visible:outline-Primary-50
+      active:not:disabled:text-Light-100
+      `,
+        disabled && `text-Dark-100`
+      ),
+      root: clsx(`_Root_  w-full min-w-[200px] text-regular-text-16 text-Light-900`, className),
     }
 
     return (
-      <div className={classes.textField}>
+      <div {...containerProps} className={classNames.root}>
         {label && (
-          <LabelPrimitive.Root className={classes.label} htmlFor={id}>
+          <Text
+            {...labelProps}
+            asComponent={'label'}
+            className={classNames.label}
+            htmlFor={finalId}
+            variant={'regular_text_16'}
+          >
             {label}
-          </LabelPrimitive.Root>
+          </Text>
         )}
-        <div className={`_InputWrapper_ relative`}>
-          <input
-            className={classes.input}
-            disabled={disabled}
-            id={id}
-            onChange={onChangeHandler}
-            onKeyDown={onKeydownHandler}
-            ref={ref}
-            type={!isVisible ? type : 'text'}
-            value={value}
-            {...rest}
-          />
-          {rest.startIcon ? (
-            <span
-              className={clsx(
-                `absolute top-1/2 text-Light-900 left-[12px] transform -translate-y-1/2 grid items-center w-[18px] h-[18px]`,
-                error &&
-                  label &&
-                  'absolute top-[18px] text-Dark-100 left-[12px] transform -translate-y-1/2 grid items-center w-[18px] h-[18px]',
-                error &&
-                  !label &&
-                  'absolute top-[19px] text-Dark-100 left-[12px] transform -translate-y-1/2 grid items-center w-[18px] h-[18px]',
-                disabled &&
-                  `absolute top-[19px] text-Dark-100 left-[12px] transform -translate-y-1/2 grid items-center w-[18px] h-[18px]
-                  cursor-not-allowed`
-              )}
-            >
-              {rest.startIcon}
-            </span>
-          ) : type === 'search' ? (
-            <div
-              className={clsx(
-                `_DivWrapper_ absolute top-1/2 left-[12px] transform -translate-y-1/2 text-Light-900
-                focus-visible:text-Light-100 focus-visible:outline focus-visible:outline-2 
-                focus-visible:outline-Primary-500
-                active:not:disabled:text-Light-100`,
-                disabled && `text-Dark-100`
-              )}
-            >
-              <SearchIcon />
+        <div className={classNames.inputContainer}>
+          {isSearchField && (
+            <div className={`top-1/4 absolute left-[3%]`}>
+              <SearchIcon
+                className={classNames.searchIcon}
+                onClick={() => inputRefOrTextareaRef.current?.focus()}
+              />
             </div>
-          ) : null}
-          {!!rest.endIcon && (
-            <span
-              className={clsx(
-                `absolute text-Light-900 top-[25%] right-[12px] transform grid items-center w-[18px] h-[18px]`,
-                error
-                  ? 'absolute text-Light-900 top-[16%] right-[12px] transform grid items-center w-[18px] h-[18px]'
-                  : null
-              )}
-            >
-              {rest.endIcon}
-            </span>
           )}
-          {type === 'password' &&
-            (isVisible ? (
-              <button
-                className={clsx(
-                  `_RightBtn_ absolute flex items-center text-Light-100/80 unset cursor-pointer top-1/2
-                 right-[12px] transform -translate-y-1/2 transition-colors ease-in-out delay-150
-                 focus:outline-none
-                 focus:ring-1 focus:ring-offset-1 focus:ring-opacity-70
-                 focus:ring-offset-Primary-500 focus:opacity-60 focus:shadow-sm focus:shadow-Primary-500
-                 `,
-                  disabled && `text-Dark-100 active:not:disabled:text-Light-100`
-                )}
-                disabled={disabled}
-                onClick={onVisible}
-                type={'button'}
-              >
-                <EyeIcon />
-              </button>
-            ) : (
-              <button
-                className={clsx(
-                  `_RightBtn_ absolute flex items-center text-Light-100/80 unset cursor-pointer top-1/2
-                right-[12px] transform -translate-y-1/2 transition-colors ease-in-out delay-150
-                focus:ring-1 focus:ring-offset-1 focus:ring-opacity-70
-                focus:ring-offset-Primary-500 focus:opacity-60 focus:shadow-sm focus:shadow-Primary-500`,
-                  disabled && `text-Dark-100/60 cursor-not-allowed`
-                )}
-                disabled={disabled}
-                onClick={onVisible}
-                type={'button'}
-              >
-                <EyeOffIcon />
-              </button>
-            ))}
-          {type === 'search' && value && (
+          <input
+            className={classNames.input}
+            id={finalId}
+            onChange={handleChange}
+            placeholder={placeholder}
+            ref={finalRef}
+            type={finalType}
+            {...restProps}
+          />
+          {isRevealPasswordButtonShown && (
             <button
               className={clsx(
-                `_RightBtn_ absolute flex items-center text-Light-100 unset cursor-pointer top-1/2
+                `_ShowPassword_ cursor-pointer absolute top-1/2 right-0 bottom-1/2
+              transform -translate-y-1/2 w-[20px] h-[20px] mr-[12px] p-0 bg-transparent border-0
+              rounder-[0.25rem] outline-0 ring-0 focus:outline-none
+              focus:ring-1 focus:ring-offset-1 focus:ring-opacity-70
+              focus:ring-offset-Primary-500 focus:opacity-60 focus:shadow-sm focus:shadow-Primary-500`,
+                disabled && `text-Dark-100 active:not:disabled:text-Light-100`
+              )}
+              onClick={handleToggleShowPassword}
+              type={'button'}
+            >
+              {revealPassword ? <EyeOffIcon /> : <EyeIcon />}
+            </button>
+          )}
+          {isClearInputButtonShown && (
+            <button
+              className={clsx(
+                `_ClearInput_ absolute flex items-center text-Light-100 unset cursor-pointer top-1/2
                 right-[12px] transform -translate-y-1/2 transition-colors ease-in-out delay-150
                 focus:ring-1 focus:ring-offset-1 focus:ring-opacity-70
                 focus:ring-offset-Primary-500 focus:opacity-60 focus:shadow-sm focus:shadow-Primary-500`,
                 disabled && `text-Dark-100`
               )}
-              disabled={disabled}
-              onClick={clearFieldHandler}
+              onClick={handleClearInput}
               type={'button'}
             >
-              <CloseIcon />
+              <CloseIcon height={20} width={20} className={`text-Primary-500`} />
             </button>
           )}
         </div>
-        {error && <span className={`text-small-text-12 text-Danger-500 mt-1`}>{error}</span>}
+
+        <Text className={classNames.error} variant={'error_text_12'} style={{ color: 'red' }}>
+          {errorMessage}
+        </Text>
       </div>
     )
   }
 )
+
+function getFinalType(
+  type: ComponentProps<'input'>['type'],
+  showPassword: boolean
+): ComponentProps<'input'>['type'] {
+  if (type === 'password' && showPassword) {
+    return 'text'
+  }
+
+  return type
+}
