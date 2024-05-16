d22f246af9301870869d4e4f7222eb290d2ccef2
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-05-17 00:01:14 +0300

    refactor: add BeakIcon for notifications-dropdown and change separator to "after"

--- src/widgets/header/notifications-dropdown/notifications-dropdown.tsx
+++ src/widgets/header/notifications-dropdown/notifications-dropdown.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from 'react'
 import { Dropdown } from '@/components/ui/drop-down-menu'
 import { MenuProps } from '@/components/ui/drop-down-menu/menu/menu'
 import { cn } from '@/utils/merge-cn'
+import { BeakIcon } from '@/assets/icons'
 
 export type NotificationsDropdownProps = {
   alternativeText?: string
@@ -33,41 +34,47 @@ export const NotificationsDropdown = ({
     ),
     header: `block pb-[12px] pl-[4px] text-regular-text-16 font-medium`,
     item: cn(
-      `flex-col w-full !items-start gap-0 !pt-[12px] !p-0 cursor-auto`,
-      `hover:!text-Light-100 hover:!bg-transparent`
+      `flex-col w-full !items-start gap-0 cursor-auto relative py-[12px]`,
+      `hover:!text-Light-100 hover:!bg-transparent`,
+      `after:content-[""] after:h-[1px] after:bg-Dark-100 after:absolute after:top-0 after:left-[4px] after:right-[4px]`
     ),
-    itemWrapper: `[&:not(:last-child)]:pb-[12px] px-[4px]`,
     longAgo: `text-small-text-12 text-Light-900`,
-    menu: `w-[335px] p-[12px] pr-[8px] bg-Dark-500`,
+    menu: `overflow-visible rounded-[4px]`,
+    wrapper: `relative overflow-visible w-[355px] p-[12px] pr-[8px] bg-Dark-500 rounded-[4px]`,
     new: `text-small-text-12 text-bold text-Primary-500`,
     subtitle: `text-sm-bold-14`,
     text: `text-regular-text-14`,
+    arrow: `absolute top-0 right-[1rem] w-[1.2rem] h-[0.6rem] translate-y-[-100%] translate-x-[50%]`,
   }
 
   return (
-    <Dropdown.Menu onOpenChange={onOpenChange} open={open} trigger={trigger} {...rest}>
-      <div className={classes.menu}>
+    <Dropdown.Menu
+      className={classes.menu}
+      onOpenChange={onOpenChange}
+      open={open}
+      trigger={trigger}
+      {...rest}
+    >
+      <div className={classes.wrapper}>
         <span className={classes.header}>Уведомления!</span>
         <div className={classes.content}>
           <div className={'pr-[4px] h-full w-full'}>
             {notifications?.length ? (
               notifications.map((item, index) => (
-                <div className={classes.itemWrapper} key={index}>
-                  <Dropdown.Separator />
-                  <Dropdown.Item className={classes.item}>
-                    <div>
-                      <span className={classes.subtitle}>{item.title}</span>
-                      {item.isNew && <span className={classes.new}>&nbsp;Новое</span>}
-                    </div>
-                    <p className={classes.text}>{item.text}</p>
-                    <span className={classes.longAgo}>{item.longAgo}</span>
-                  </Dropdown.Item>
-                </div>
+                <Dropdown.Item className={classes.item} key={index}>
+                  <div>
+                    <span className={classes.subtitle}>{item.title}</span>
+                    {item.isNew && <span className={classes.new}>&nbsp;Новое</span>}
+                  </div>
+                  <p className={classes.text}>{item.text}</p>
+                  <span className={classes.longAgo}>{item.longAgo}</span>
+                </Dropdown.Item>
               ))
             ) : (
               <span className={classes.alternativeText}>{alternativeText}</span>
             )}
           </div>
+          <BeakIcon className={classes.arrow} />
         </div>
       </div>
     </Dropdown.Menu>
