078e9d6a359453fb742c89d33798023fefc446f9
Alex666000 <bogdanov777000@mail.ru>
2024-05-16 13:24:10 +0300

    refactor

--- src/components/ui/tabs/tabs.tsx
+++ src/components/ui/tabs/tabs.tsx
@@ -5,9 +5,7 @@ import * as Tabs from '@radix-ui/react-tabs'
 
 /** Стили для активного и неактивного состояния табов */
 const baseClasses = `w-full h-fit bg-transparent text-nowrap font-H3-16 border-b-2 
-
 data-[state='active']:text-Primary-500 data-[state='active']:border-b-Primary-500 disabled:data-[state='active']:text-Primary-900 disabled:data-[state='active']:border-b-Primary-900 hover:data-[state='active']:bg-Primary-900 hover:data-[state='active']:bg-opacity-15 active:data-[state='active']:bg-Primary-100 active:data-[state='active']:bg-opacity-15 active:data-[state='active']:outline-none focus-visible:data-[state='active']:outline-none focus-visible:data-[state='active']:ring-2 focus-visible:data-[state='active']:rounded-sm focus-visible:data-[state='active']:ring-Primary-300
-
 data-[state='inactive']:text-Dark-100 data-[state='inactive']:border-b-Dark-100 disabled:data-[state='inactive']:text-Dark-300 disabled:data-[state='inactive']:border-b-Dark-300 focus-visible:data-[state='inactive']:outline-none focus-visible:data-[state='inactive']:ring-2 focus-visible:data-[state='inactive']:rounded-sm focus-visible:data-[state='inactive']:ring-Primary-300 hover:data-[state='inactive']:bg-Primary-900 hover:data-[state='inactive']:bg-opacity-15 active:data-[state='inactive']:bg-Primary-100 active:data-[state='inactive']:bg-opacity-15 active:data-[state='inactive']:outline-none active:ring-0`
 
 const sizes = {
@@ -16,7 +14,7 @@ const sizes = {
 
 export type Tab = {
   disabled?: boolean
-  label: string
+  title: string
   value: string
 }
 
@@ -30,26 +28,38 @@ type TabsProps = {
 
   size?: 'base'
   /** Массив с названиями табов, их значением и состоянием */
-  tabs: Tab[]
+  tabsValues: Tab[]
   triggerClassName?: string
+  ariaLabel?: string
+  fullWidth?: boolean
 } & Omit<ComponentPropsWithoutRef<typeof Tabs.Root>, 'asChild'>
 
 const TabSwitcher = forwardRef<HTMLButtonElement, TabsProps>((props, tabsTriggerRef) => {
-  const { children, contentClassName, listClassName, size, tabs, triggerClassName, ...rest } = props
+  const {
+    children,
+    contentClassName,
+    listClassName,
+    size,
+    tabsValues,
+    fullWidth,
+    triggerClassName,
+    ariaLabel,
+    ...rest
+  } = props
 
-  const contentRef = useRef(null)
+  const contentRef = useRef<HTMLDivElement>(null)
 
   const classes = {
     content: cn('w-full', contentClassName),
     /** Табы растягиваются на всю ширину контейнера и скролятся, если их ширина превышает ширину контейнера */
     list: cn('flex w-full overflow-x-scroll', listClassName),
-    trigger: cn([baseClasses, sizes[size || 'base'], triggerClassName]),
+    trigger: cn([baseClasses, sizes[size || 'base'], fullWidth && `w-full`, triggerClassName]),
   }
 
   return (
-    <Tabs.Root defaultValue={tabs[0].value} {...rest}>
-      <Tabs.List className={classes.list}>
-        {tabs.map((tab, index) => (
+    <Tabs.Root defaultValue={tabsValues[0].value} {...rest}>
+      <Tabs.List className={classes.list} aria-label={ariaLabel || 'tab-switcher'}>
+        {tabsValues.map((tab, index) => (
           <Tabs.Trigger
             className={classes.trigger}
             disabled={tab.disabled}
@@ -57,13 +67,18 @@ const TabSwitcher = forwardRef<HTMLButtonElement, TabsProps>((props, tabsTrigger
             ref={tabsTriggerRef}
             value={tab.value}
           >
-            {tab.label}
+            {tab.title}
           </Tabs.Trigger>
         ))}
       </Tabs.List>
 
       {React.Children.map(children, (child, i) => (
-        <Tabs.Content ref={contentRef} className={classes.content} key={i} value={tabs[i].value}>
+        <Tabs.Content
+          ref={contentRef}
+          className={classes.content}
+          key={i}
+          value={tabsValues[i].value}
+        >
           {child}
         </Tabs.Content>
       ))}
--- src/utils/merge-refs.ts
+++ src/utils/merge-refs.ts
@@ -2,6 +2,9 @@
 
 import { LegacyRef, MutableRefObject, RefCallback } from 'react'
 
+/**
+ * "Мержит" рефы если используем свой реф + реф созданный и переданный родителем
+ */
 export function mergeRefs<T = any>(
   refs: Array<LegacyRef<T> | MutableRefObject<T> | null | undefined>
 ): RefCallback<T> {
