942dd7912d55c6563f14cb7405cda386f5f8ac57
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-05-17 19:15:26 +0300

    feat: add alert component

--- src/components/ui/alert/alert.tsx
+++ src/components/ui/alert/alert.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react'
-import { cva, type VariantProps } from 'class-variance-authority'
-import { cn } from '@/utils/merge-cn'
-import { ComponentPropsWithoutRef, forwardRef, useEffect, useRef, useState } from 'react'
+import { ComponentPropsWithoutRef } from 'react'
+
 import { CrossIcon } from '@/assets/icons'
+import { useAlert } from '@/components/ui/alert/hooks'
+import { cn } from '@/utils/merge-cn'
+import { type VariantProps, cva } from 'class-variance-authority'
 
 const alertVariants = cva('relative w-full rounded-[2px] border px-[1.5rem] py-[0.8rem]', {
   variants: {
     variant: {
-      success: 'bg-Success-900 border-Success-500',
       danger: 'bg-Danger-900 border-Danger-500',
+      success: 'bg-Success-900 border-Success-500',
     },
   },
 })
@@ -17,47 +19,46 @@ type Props = {
   text: string
 } & ComponentPropsWithoutRef<'div'>
 
-export const Alert = forwardRef<HTMLDivElement, Props & VariantProps<typeof alertVariants>>(
-  ({ variant, text, className, ...rest }, ref) => {
-    const classes = {
-      error: ``,
-      closeButton: `flex justify-center items-center absolute w-[24px] h-[24px] right-[1.5rem] top-[0.7rem]`,
-    }
-
-    const modalRef = useRef<HTMLDivElement | null>(null)
-
-    const handleClickOutside = (event: MouseEvent) => {
-      if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
-        setOpen(false)
-      }
-    }
-
-    useEffect(() => {
-      document.addEventListener('mousedown', handleClickOutside)
-      return () => {
-        document.removeEventListener('mousedown', handleClickOutside)
-      }
-    }, [])
-
-    const [open, setOpen] = useState(true)
-
-    return open ? (
-      <div
-        ref={modalRef}
-        role="alert"
-        className={cn(alertVariants({ variant }), className)}
-        {...rest}
-      >
-        {variant === 'danger' && <span>Error! </span>}
-        {text}
-        <button className={classes.closeButton} onClick={() => setOpen(false)}>
-          <CrossIcon />
-        </button>
-      </div>
-    ) : (
-      <></>
-    )
+export const Alert = ({
+  className,
+  text,
+  variant,
+  ...rest
+}: Props & VariantProps<typeof alertVariants>) => {
+  const classes = {
+    closeButton: cn(
+      `flex justify-center items-center absolute !w-[24px] h-[24px] right-[1.5rem] top-[0.7rem]`,
+      `rounded-[4px] border-2 border-transparent outline-none`,
+      `transition-opacity transition-transform duration-300`,
+      `hover:translate-y-[-4px] active:opacity-50`,
+      `focus-visible:border-Light-100`
+    ),
+    error: `font-bold`,
   }
-)
+
+  const { closeHandler, modalRef, open } = useAlert()
+
+  return open ? (
+    <div
+      className={cn(alertVariants({ variant }), className)}
+      ref={modalRef}
+      role={'alert'}
+      {...rest}
+    >
+      {variant === 'danger' && <span className={classes.error}>Error! </span>}
+      {text}
+      <button
+        aria-label={'Close alert'}
+        className={classes.closeButton}
+        onClick={closeHandler}
+        type={'button'}
+      >
+        <CrossIcon />
+      </button>
+    </div>
+  ) : (
+    <></>
+  )
+}
 
 Alert.displayName = 'Alert'
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,14 @@
+import { useState } from 'react'
+
 import { getLayout } from '@/components/Layout/Layout'
+import { Alert } from '@/components/ui/alert'
 import { PageWrapper } from 'src/components/ui/page-wrapper'
 
 const Home = () => {
   return (
     <PageWrapper title={'Home | Instagram'}>
       <h1>Home</h1>
+      <Alert text={'test'} variant={'danger'}></Alert>
     </PageWrapper>
   )
 }
