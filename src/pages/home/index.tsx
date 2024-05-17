93350449730f6c05067e65c1d93ac49b80c80271
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-05-17 19:43:25 +0300

    chore: remove unnecessary code

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-
 import { getLayout } from '@/components/Layout/Layout'
-import { Alert } from '@/components/ui/alert'
 import { PageWrapper } from 'src/components/ui/page-wrapper'
 
 const Home = () => {
   return (
     <PageWrapper title={'Home | Instagram'}>
       <h1>Home</h1>
-      <Alert text={'test'} variant={'danger'}></Alert>
     </PageWrapper>
   )
 }
