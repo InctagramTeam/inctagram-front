04f1be23a7539f31c26e9ea1b483bf33401916d7
Alex666000 <bogdanov777000@mail.ru>
2024-05-17 08:31:10 +0300

    refactor check

--- src/components/ui/Checkbox_/Checkbox.tsx
+++ src/components/ui/Checkbox_/Checkbox.tsx
@@ -1,31 +1,56 @@
-import React, { ComponentPropsWithoutRef, ElementRef, forwardRef, ReactNode } from 'react'
+import React, { ComponentPropsWithoutRef, ElementRef, forwardRef, ReactNode, useId } from 'react'
 
 import * as CheckboxRadix from '@radix-ui/react-checkbox'
+import * as LabelRadix from '@radix-ui/react-label'
 import clsx from 'clsx'
 import CheckIcon from '@/assets/icons/CheckIcon'
+import { ReturnComponent } from '@/common/types'
 
 export type CheckboxProps = {
+  checked?: boolean
+  className?: string
+  disabled?: boolean
   label?: ReactNode | string
-} & ComponentPropsWithoutRef<typeof CheckboxRadix.Root>
+  id?: string
+  labelPosition?: 'left' | 'right'
+  value?: string
+  onValueChange?: (checked: boolean) => void
+  required?: boolean
+} & Omit<ComponentPropsWithoutRef<typeof CheckboxRadix.Root>, 'checked' | 'onCheckedChange'>
 
 export type Ref = ElementRef<typeof CheckboxRadix.Root>
 
-export const Checkbox = forwardRef<Ref, CheckboxProps>(
-  ({ className, disabled = true, label = 'DDDDDD', ...rest }, ref) => {
-    return (
-      <div className={`_container_ flex items-center`}>
-        <label
+export const Checkbox = forwardRef<Ref, CheckboxProps>((props, ref): ReturnComponent => {
+  const {
+    className,
+    disabled,
+    label = '',
+    checked,
+    id,
+    labelPosition = 'right',
+    name,
+    onValueChange,
+    required,
+    ...rest
+  } = props
+
+  const generatedId = useId()
+  const finalId = id ?? generatedId
+
+  return (
+    <div className={`_container_ flex items-center`}>
+      <LabelRadix.Root
+        htmlFor={finalId}
+        className={clsx(
+          `_label_ select-none z-0 inline-flex gap-[11px] items-center text-regular-text-14 cursor-default`,
+          className,
+          disabled && `text-Light-900/60`
+        )}
+      >
+        <CheckboxRadix.Root
           className={clsx(
-            `_label_ select-none z-0 inline-flex gap-[11px] items-center
-            text-regular-text-14 text-Light-100 disabled:text-Dark-100
-            hover:translate-y-[1px]
-            `,
-            disabled && `_disabled_ cursor-default text-Light-900`,
-            className
-          )}
-        >
-          <CheckboxRadix.Root
-            className={`_Checkbox_ relative w-[18px] h-[18px] p-[3px_1px] bg-transparent cursor-pointer flex shrink-0 items-center justify-center
+            disabled && `cursor-default`,
+            `_Checkbox_ relative w-[18px] h-[18px] p-[3px_1px] bg-transparent cursor-pointer flex shrink-0 items-center justify-center
             border-2 border-Light-500 rounded
             before:content-['']
             before:absolute before:-z-1 before:w-[32px] before:h-[32px] before:opacity-0
@@ -38,33 +63,37 @@ export const Checkbox = forwardRef<Ref, CheckboxProps>(
             focus-visible:not:data-[disabled]:before:opacity-100
             focus-visible:not:data-[disabled]:before:bg-Dark-500
             active:not:data-[disabled]:before:bg-Dark-100
-            disabled:bg-Dark-700
+            hover:disabled:cursor-default hover:disabled:before:hidden
             data-[state=checked]:bg-Light-100 data-[disabled]:cursor-default 
             data-[disabled]:bg-Dark-100 data-[disabled]:border-2 data-[disabled]:border-Light-900 data-[disabled]:opacity-30
             data-[state=checked]:cursor-pointer data-[state=checked,disabled]:bg-Dark-100
             data-[state=checked]:border-2 data-[state=checked]:border-Dark-100
             data-[state=unchecked]:cursor-pointer data-[state=unchecked]:border-2 data-[state=unchecked]:border-Dark-100
             data-[state=unchecked]:bg-Dark-700
-            `}
-            disabled={disabled}
-            ref={ref}
-            {...rest}
+            `
+          )}
+          id={id}
+          checked={checked}
+          name={name}
+          disabled={disabled}
+          required={required}
+          ref={ref}
+          {...rest}
+        >
+          <CheckboxRadix.Indicator
+            className={`_indicator_ flex items-center justify-center data-[disabled]:cursor-default`}
           >
-            <CheckboxRadix.Indicator
-              className={`_indicator_ flex items-center justify-center  data-[disabled]:cursor-default`}
-            >
-              <CheckIcon
-                className={
-                  disabled
-                    ? `_checkIconDisabled_ text-Light-700`
-                    : `_checkIcon_ text-Dark-900  data-[disabled]:cursor-default`
-                }
-              />
-            </CheckboxRadix.Indicator>
-          </CheckboxRadix.Root>
-          {label}
-        </label>
-      </div>
-    )
-  }
-)
+            <CheckIcon
+              className={
+                disabled
+                  ? `_checkIconDisabled_ text-Light-700 cursor-default`
+                  : `_checkIcon_ text-Dark-900`
+              }
+            />
+          </CheckboxRadix.Indicator>
+        </CheckboxRadix.Root>
+        {label}
+      </LabelRadix.Root>
+    </div>
+  )
+})
