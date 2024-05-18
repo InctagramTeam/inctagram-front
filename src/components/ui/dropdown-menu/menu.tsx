7bc9ae30ca1c8a23ce2dd7203497fe06a55ab609
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-05-18 14:58:53 +0300

    fix: correct menu in dropdown menu

--- src/components/ui/dropdown-menu/menu.tsx
+++ src/components/ui/dropdown-menu/menu.tsx
@@ -1,46 +1,48 @@
-import { ComponentPropsWithoutRef, CSSProperties, ReactNode, forwardRef } from 'react'
-import * as DropdownRadix from '@radix-ui/react-dropdown-menu'
+import { CSSProperties, ComponentPropsWithoutRef, ReactNode, forwardRef } from 'react'
+
 import { ReturnComponent } from '@/common/types'
+import * as DropdownRadix from '@radix-ui/react-dropdown-menu'
 import { clsx } from 'clsx'
 
-type MenuProps = {
-  trigger: ReactNode
+export type MenuProps = {
+  /** Выравнивание относительно триггера (кнопки) */
+  align?: 'center' | 'end' | 'start'
+  className?: string
+  modal?: boolean
+  onOpenChange?: (open: boolean) => void
+  open?: boolean
   /**
    * onPointerDownOutside - Обработчик события мыши или "касание" пальцем экрана,
    * вызываемый при возникновении события с указателем за пределами компонента.
    * Его можно предотвратить, вызвав event.preventDefault.
    */
   portal?: boolean
-  /** Выравнивание относительно триггера (кнопки) */
-  align?: 'center' | 'end' | 'start'
   /** Расстояние в "px" от trigger-a */
   sideOffset?: number
-  className?: string
-  modal?: boolean
   style?: CSSProperties
-  open?: boolean
-  onOpenChange?: (open: boolean) => void
+  trigger: ReactNode
 } & ComponentPropsWithoutRef<typeof DropdownRadix.Content>
 
 const Menu = forwardRef<HTMLDivElement, MenuProps>((props, ref): ReturnComponent => {
   const {
-    children,
-    portal = true,
     align = 'start',
-    style,
+    children,
     className,
-    sideOffset = 0,
-    trigger,
     modal,
-    open,
     onOpenChange,
+    open,
+    portal = true,
+    sideOffset = 0,
+    style,
+    trigger,
     ...rest
   } = props
 
   const menuContent = (
     <DropdownRadix.Content
+      align={align}
       className={clsx(
-        `relative overflow-visible mt-[10px]
+        `relative overflow-visible
          border border-Dark-100 rounded-[10px] p-[5px] pb-0 bg-Dark-500 text-Light-100 shadow-sm
          before:content-[''] before:absolute before:bg-Dark-500 before:left-[10px] before:w-[10px] before:h-[10px]
          before:-top-[6px] before:shadow-[0_07px_Dark-500] before:rotate-45 before:z-1
@@ -48,8 +50,8 @@ const Menu = forwardRef<HTMLDivElement, MenuProps>((props, ref): ReturnComponent
          before:border-l-[1px] before:border-t-[1px] before:rounded-xs`,
         className
       )}
-      align={align}
       ref={ref}
+      sideOffset={sideOffset}
       {...rest}
       onPointerDownOutside={e => {
         if (!portal) {
@@ -63,7 +65,9 @@ const Menu = forwardRef<HTMLDivElement, MenuProps>((props, ref): ReturnComponent
 
   return (
     <DropdownRadix.Root modal={modal} onOpenChange={onOpenChange} open={open}>
-      <DropdownRadix.Trigger className={'outline-none'}>{trigger}</DropdownRadix.Trigger>
+      <DropdownRadix.Trigger asChild className={'outline-none'}>
+        {trigger}
+      </DropdownRadix.Trigger>
       {portal ? <DropdownRadix.Portal>{menuContent}</DropdownRadix.Portal> : menuContent}
     </DropdownRadix.Root>
   )
