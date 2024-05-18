1477272b7c5559c7ba8bb513010e4f1cdf245b19
openservicedv <150409269+openservicedv@users.noreply.github.com>
2024-05-18 09:18:03 -0700

    IN-56 fix

--- src/components/Card/Cards.tsx
+++
@@ -1,7 +0,0 @@
-import { Card } from '@/components/ui/card/card'
-
-export const Cards = () => {
-  return (
-    <Card className={'w-[100px] h-[100px] bg-[#171717] border border-[#333333] rounded-sm'}></Card>
-  )
-}
