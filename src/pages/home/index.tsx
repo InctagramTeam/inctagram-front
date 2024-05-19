d8e5190a1f267781dd9f78ac5ceadd7383b418b8
openservicedv <150409269+openservicedv@users.noreply.github.com>
2024-05-18 17:47:10 -0700

    IN-51(1): feat

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,13 @@
 import { getLayout } from '@/components/Layout/Layout'
 import { PageWrapper } from 'src/components/ui/page-wrapper'
+import * as React from 'react'
+import { SelectBox } from '@/components/ui/select/select'
 
 const Home = () => {
   return (
     <PageWrapper title={'Home | Instagram'}>
       <h1>Home</h1>
+      <SelectBox title={'select'} disabled={false} />
     </PageWrapper>
   )
 }
