9632315182e3a72985356b44bba0f3155ccd36c3
openservicedv <150409269+openservicedv@users.noreply.github.com>
2024-05-20 09:16:50 -0700

    IN-50(1) fix

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,12 @@
 import { PageWrapper } from '@/shared/layouts/page-wrapper'
 import { getLayout } from '@/shared/layouts/layout'
+import { Recaptcha } from '@/shared/ui/recaptcha/recaptcha'
 
 const Home = () => {
   return (
     <PageWrapper title={'Home | Instagram'}>
       <h1>Home</h1>
+      <Recaptcha error={false} expired={true} state={'default'} />
     </PageWrapper>
   )
 }
