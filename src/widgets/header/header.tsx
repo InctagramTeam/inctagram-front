458063ff1c3435769a8d5984d37b61dd20ce8ccd
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-05-24 18:54:23 +0300

    chore: add header-menu

--- src/widgets/header/header.tsx
+++ src/widgets/header/header.tsx
@@ -1,17 +1,12 @@
-import { useEffect, useState } from 'react'
 import * as React from 'react'
+import { useState } from 'react'
 
-import { cn } from '@/shared/lib/utils'
 import { Flex } from '@/shared/ui/flex'
-import {
-  AuthButtons,
-  Logo,
-  MobileDropdown,
-  NotificationProps,
-  NotificationsDropdown,
-} from '@/widgets/header/ui'
-import { LanguageSelection } from '@/widgets/header/ui/language-selection/language-selection'
-import { SelectItemProps } from '@radix-ui/react-select'
+import { languages } from '@/widgets/header/model/constants'
+import { HeaderMenu } from '@/widgets/header/ui/header-menu'
+import { Logo } from '@/widgets/header/ui/logo'
+import { NotificationProps } from '@/widgets/header/ui/notifications-dropdown-list'
+import { clsx } from 'clsx'
 
 type Props = {
   className?: string
@@ -20,81 +15,20 @@ type Props = {
   notifications?: NotificationProps[]
 }
 
-const languages: SelectItemProps[] = [
-  {
-    textValue: 'English',
-    value: 'english',
-  },
-  {
-    textValue: 'Russia',
-    value: 'russia',
-  },
-]
-
-export const Header = ({ className, logout, notifications, ...rest }: Props) => {
+export const Header = ({ className, ...rest }: Props) => {
   const classes = {
-    button: `py-[6px] text-center !text-H3-16 hover:translate-y-0`,
-    countNotifications: cn(
-      `bg-Danger-500 text-Light-100 rounded-full w-[13px] h-[13px] flex items-center justify-center`,
-      `absolute top-0 right-0 text-[0.6rem] leading-[0.5rem]`
+    header: clsx(
+      `fixed inset-0 border-b-[1px] shadow-sm shadow-Dark-300 border-Dark-100 w-full h-[60px] py-[15px] z-10 bg-Dark-700`,
+      className
     ),
-    dropdownTrigger: cn(
-      `w-[24px] !h-[24px] justify-center hover:translate-y-0 !p-0 relative`,
-      `hover:bg-Primary-500 active:opacity-50 duration-300 transition-opacity`
-    ),
-    header: `fixed inset-0 border-b-[1px] shadow-sm shadow-Dark-300 border-Dark-100 w-full h-[60px] py-[15px] z-10 bg-Dark-700`,
-    loginLink: cn(
-      `px-[26px] !text-Primary-500 duration-300`,
-      `hover:no-underline hover:!text-Primary-100`,
-      `active:!text-Primary-700`,
-      `focus:no-underline`
-    ),
-    signupLink: `px-[20px]`,
     wrapper: `max-w-[1186px] w-full mx-auto px-[15px]`,
   }
-
-  const [width, setWidth] = useState<null | number>(null)
-  const mobileBreakpoint = 768
-
-  const [value, setValue] = useState(languages[0].value)
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      setWidth(window.innerWidth)
-      const handleWindowResize = () => setWidth(window.innerWidth)
-
-      window.addEventListener('resize', handleWindowResize)
-
-      return () => window.removeEventListener('resize', handleWindowResize)
-    }
-  }, [])
-
-  if (width === null) {
-    return null
-  }
-
-  const isDesktop = width > mobileBreakpoint
 
   return (
     <header className={classes.header}>
       <Flex className={classes.wrapper} gap={'20'} items={'center'} justify={'spaceBetween'}>
         <Logo />
-        <Flex className={'flex-nowrap'} gap={'40'}>
-          {isDesktop && <NotificationsDropdown notifications={notifications} />}
-          <LanguageSelection
-            isMobile={!isDesktop}
-            items={languages}
-            onValueChange={setValue}
-            value={value}
-          />
-          {isDesktop ? (
-            <Flex className={'flex-nowrap'} gap={'24'}>
-              <AuthButtons />
-            </Flex>
-          ) : (
-            <MobileDropdown logout={logout} />
-          )}
-        </Flex>
+        <HeaderMenu {...rest} />
       </Flex>
     </header>
   )
---
+++ src/widgets/header/ui/header-menu/header-menu.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react'
+import { useState } from 'react'
+
+import { MD_BREAKPOINT } from '@/shared/constants'
+import { useResponsive } from '@/shared/lib/hooks'
+import { Flex } from '@/shared/ui/flex'
+import { languages } from '@/widgets/header/model/constants'
+import { AuthButtons } from '@/widgets/header/ui/auth-buttons'
+import { LanguageSelection } from '@/widgets/header/ui/language-selection'
+import { MobileDropdown } from '@/widgets/header/ui/mobile-dropdown'
+import { NotificationsDropdown } from '@/widgets/header/ui/notifications-dropdown'
+import { NotificationProps } from '@/widgets/header/ui/notifications-dropdown-list'
+
+type Props = {
+  isAuth?: boolean
+  logout?: () => void
+  notifications?: NotificationProps[]
+}
+export const HeaderMenu = ({ isAuth, logout, notifications }: Props) => {
+  const { width } = useResponsive()
+  const [value, setValue] = useState(languages[0].value)
+
+  if (width === null) {
+    return null
+  }
+
+  const isDesktop = width > MD_BREAKPOINT
+
+  return (
+    <Flex className={'flex-nowrap'} gap={'40'}>
+      {isDesktop && <NotificationsDropdown notifications={notifications} />}
+      <LanguageSelection
+        isMobile={!isDesktop}
+        items={languages}
+        onValueChange={setValue}
+        value={value}
+      />
+      {isDesktop && !isAuth && (
+        <Flex className={'flex-nowrap'} gap={'24'}>
+          <AuthButtons />
+        </Flex>
+      )}
+      {!isDesktop && <MobileDropdown logout={logout} />}
+    </Flex>
+  )
+}
