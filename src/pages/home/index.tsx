874a8c99ddca5c1bdb3380e9d1f22788d23d5750
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-05-28 12:41:38 +0300

    refactor: remove popover in datepicker and add popover from share/ui

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,7 +2,6 @@ import { getLayout } from '@/shared/layouts/layout'
 import { PageWrapper } from '@/shared/layouts/page-wrapper'
 
 const Home = () => {
-
   return (
     <PageWrapper title={'Home | Instagram'}>
       <h1>Home</h1>
