8a05ac70e2d3218ed9b0073b913da708af9a6256
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-05-29 19:05:24 +0300

    refactor: use ul and li in mobile-dropdown

--- src/widgets/header/ui/mobile-dropdown/mobile-dropdown.tsx
+++ src/widgets/header/ui/mobile-dropdown/mobile-dropdown.tsx
@@ -11,7 +11,8 @@ type Props = {
 
 export const MobileDropdown = ({ logout }: Props) => {
   const classes = {
-    item: `mb-[12px] last:mb-[0] data-[highlighted]:ring-Primary-700 data-[highlighted]:ring-1 data-[highlighted]:outline-none rounded-[2px]`,
+    items: `flex flex-col gap-[12px] mb-[12px]`,
+    item: `w-full data-[highlighted]:ring-Primary-700 data-[highlighted]:ring-1 data-[highlighted]:outline-none rounded-[2px]`,
     link: `bg-transparent h-auto flex gap-[12px] py-[6px] px-[12px] transition duration-300 outline-none rounded-[2px] !text-regular-text-14
     active:opacity-60 focus:ring-1 focus:ring-Primary-700 focus-visible:ring-1 focus-visible:ring-Primary-700`,
     logoutButton: `h-auto !justify-normal bg-transparent focus:bg-transparent hover:translate-y-0 active:bg-transparent active:!text-Light-100`,
@@ -28,21 +29,25 @@ export const MobileDropdown = ({ logout }: Props) => {
   return (
     <Dropdown.Menu className={classes.menu} trigger={trigger}>
       <div>
-        {LINKS.map((link, index) => {
-          return (
-            <Dropdown.Item className={classes.item} key={index}>
-              <Button
-                asComponent={Link}
-                className={classes.link}
-                href={link.href}
-                variant={'text'}
-                startIcon={getIcon(link.href, false)}
-              >
-                {link.name}
-              </Button>
-            </Dropdown.Item>
-          )
-        })}
+        <ul className={classes.items}>
+          {LINKS.map((link, index) => {
+            return (
+              <Dropdown.Item className={classes.item} key={index}>
+                <li>
+                  <Button
+                    asComponent={Link}
+                    className={classes.link}
+                    href={link.href}
+                    variant={'text'}
+                    startIcon={getIcon(link.href, false)}
+                  >
+                    {link.name}
+                  </Button>
+                </li>
+              </Dropdown.Item>
+            )
+          })}
+        </ul>
         <Dropdown.Item className={classes.item}>
           <Button
             startIcon={<LogOutIcon />}
