d51b3e637ab5fb6b650bd6d24706eea92ca7eb63
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-06-06 12:51:26 +0300

    refactor: use navigation-element in sidebar

--- src/widgets/sidebar/ui/sidebar-list/sidebar-list.tsx
+++ src/widgets/sidebar/ui/sidebar-list/sidebar-list.tsx
@@ -3,15 +3,19 @@ import React from 'react'
 import { useLayoutContext } from '@/shared/layouts'
 import { cn } from '@/shared/lib/utils'
 import { NavLink } from '@/shared/types'
-import { SidebarLink } from '@/widgets/sidebar/ui'
+import { NavigationElement } from '@/shared/ui/navigation-element/navigation-element'
+import Link from 'next/link'
 
 type Props = {
   isMobile?: boolean
+  onlyIcons?: boolean
   links: NavLink[]
 }
 
-export const SidebarList = ({ isMobile = false, links }: Props) => {
+export const SidebarList = ({ isMobile = false, onlyIcons = false, links }: Props) => {
   const { isCollapsed } = useLayoutContext()
+
+  console.log(isCollapsed)
 
   const classes = {
     item: cn(isMobile && `w-[24px] h-[24px]`, !isMobile && 'flex'),
@@ -19,7 +23,7 @@ export const SidebarList = ({ isMobile = false, links }: Props) => {
       'w-full flex cursor-pointer rounded-md transition-colors',
       !isMobile && 'mb-[46px] flex-col gap-[24px] [&>*:nth-child(5)]:mb-[46px]',
       (isCollapsed || isMobile) && 'justify-center',
-      isCollapsed && 'items-center',
+      (isCollapsed || onlyIcons) && 'items-center',
       isMobile && 'gap-[36px]'
     ),
   }
@@ -29,7 +33,7 @@ export const SidebarList = ({ isMobile = false, links }: Props) => {
       {links.map(link => {
         return (
           <li className={classes.item} key={link.href}>
-            <SidebarLink isCollapsed={isCollapsed} link={link} />
+            <NavigationElement asComponent={Link} onlyIcon={onlyIcons} {...link} />
           </li>
         )
       })}
