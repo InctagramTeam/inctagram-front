595ee9b2a38bc5419acf72e5e1c4da62064bfefc
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-06-07 22:26:47 +0300

    add modal in sign-up page

--- src/pages/auth/sign-up/index.tsx
+++ src/pages/auth/sign-up/index.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 import { SignUpFormValues } from '@/feature/auth/model/utils/validators/sign-up-validation-schema'
 import { SignUpForm } from '@/feature/auth/ui/sign-up-form/sign-up-form'
@@ -7,9 +7,13 @@ import { PageWrapper } from '@/shared/layouts/page-wrapper'
 import { ReturnComponent } from '@/shared/types'
 import { UseFormRef } from '@/shared/types/form'
 import { getAuthLayout } from '@/shared/layouts/auth-layout/auth-layout'
+import { Modal } from '@/shared/ui/modal'
+import { Button } from '@/shared/ui/button'
+import { Flex } from '@/shared/ui/flex'
 
 const SignUp = (): ReturnComponent => {
   const ref = useRef<UseFormRef<SignUpFormValues>>(null)
+  const [open, setOpen] = useState(false)
 
   const handleSubmitForm = ({ accept, passwordConfirm, ...formData }: SignUpFormValues) => {
     // ref?.current?.reset()
@@ -24,6 +28,16 @@ const SignUp = (): ReturnComponent => {
         onSubmit={handleSubmitForm}
         ref={ref}
       />
+      <Modal open={open} onOpenChange={setOpen}>
+        <Modal.Content title={`Email sent`} classNameContent={'max-w-[378px]'}>
+          <Flex direction={'column'} gap={'18'}>
+            <p>We have sent a link to confirm your email to epam@epam.com</p>
+            <Button className={'ml-auto px-[36px] py-[6px]'} onClick={() => setOpen}>
+              OK
+            </Button>
+          </Flex>
+        </Modal.Content>
+      </Modal>
     </PageWrapper>
   )
 }
