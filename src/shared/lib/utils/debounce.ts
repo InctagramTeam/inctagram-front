c3517f81b978fd63e8eab5a6a49f6963946c16b0
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-06-16 13:37:24 +0300

    add debounce util

---
+++ src/shared/lib/utils/debounce.ts
@@ -0,0 +1,43 @@
+/**
+ * Функция debounce первым аргументов принимает функцию, а вторым время (в ms)
+ * переднная функция будет вызываться не раньше указанного промежутка времени
+ * после последнего вызова.
+ * Возвращает объект с заторможенной функцией и функцией отмены.
+ * */
+
+export function debounce<T extends (...args: unknown[]) => void>(
+  func: T,
+  delay: number
+): { debouncedFunction: (...args: Parameters<T>) => void; cancel: () => void } {
+  let timeoutId: ReturnType<typeof setTimeout> | null = null
+
+  const debouncedFunction = function (...args: Parameters<T>): void {
+    if (timeoutId) {
+      /** Если предыдущий таймер был установлен, то отменяем его */
+      clearTimeout(timeoutId)
+    }
+    /** Устанавливаем новый таймер, который вызовет func после задержки */
+    timeoutId = setTimeout(() => func(...args), delay)
+  }
+
+  /** Функция для отмены вызова, если он был запланирован */
+  const cancel = () => {
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+  }
+
+  return { debouncedFunction, cancel }
+}
+
+/**
+ * Пример
+ * function saveInput(inputValue: string) {
+ *   console.log(`Сохранение данных: ${inputValue}`);
+ * }
+ *
+ * const { debouncedFunction, cancel } = debounce(saveInput, 1000);
+ * function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+ *   debouncedFunction(event.target.value);
+ * }
+ * */
