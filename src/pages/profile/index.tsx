9cd3125de9f832b7dd65cca0fc790638d7fa3e31
Alex666000 <bogdanov777000@mail.ru>
2024-06-19 19:58:21 +0300

    add skeleton

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,9 +1,18 @@
 import { PageWrapper, getBaseAppLayout } from '@/shared/layouts'
+import { Avatar } from '@/shared/ui/avatar'
 
 const Profile = () => {
   return (
-    <PageWrapper title={'Profile | Instagram'}>
-      <h1>Profile</h1>
+    <PageWrapper title={'Profile | Instagram'} paddingBlock={'36xp'}>
+      <div className={`Profile`}>
+        <div className={`profile-top`}>
+          <div className={`avatar-photo`}>
+            <Avatar />
+          </div>
+          <Text asComponent="h2" className={`URL_Profile`} variant="h1"></Text>
+        </div>
+        <div className={`profile-top`}></div>
+      </div>
     </PageWrapper>
   )
 }
