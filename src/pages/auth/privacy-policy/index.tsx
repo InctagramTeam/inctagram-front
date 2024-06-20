3de93bbee086181cedabae5654c9bfc413f39f40
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-06-20 23:25:16 +0300

    add meta-title and meta-description for privacy-police and terms-of-service

--- src/pages/auth/privacy-policy/index.tsx
+++ src/pages/auth/privacy-policy/index.tsx
@@ -1,8 +1,14 @@
-import { PageWrapper, getAuthLayout } from '@/shared'
+import { PageWrapper, getAuthLayout, useTranslation } from '@/shared'
 
 const PrivacyPolicy = () => {
+  const { t } = useTranslation()
+
   return (
-    <PageWrapper paddingBlock={'72px'} title={'PrivacyPolicy | Instagram'}>
+    <PageWrapper
+      paddingBlock={'72px'}
+      title={t.pages.privacyPolice.metaTitle}
+      description={t.pages.privacyPolice.metaDescription}
+    >
       <h1>PrivacyPolicy</h1>
     </PageWrapper>
   )
--- src/pages/auth/term-of-service/index.tsx
+++ src/pages/auth/term-of-service/index.tsx
@@ -1,8 +1,14 @@
-import { PageWrapper, getAuthLayout } from '@/shared'
+import { PageWrapper, getAuthLayout, useTranslation } from '@/shared'
 
 const TermOfService = () => {
+  const { t } = useTranslation()
+
   return (
-    <PageWrapper paddingBlock={'72px'} title={'TermOfService | Instagram'}>
+    <PageWrapper
+      paddingBlock={'72px'}
+      title={t.pages.termsOfService.metaTitle}
+      description={t.pages.termsOfService.metaDescription}
+    >
       <h1>TermOfService</h1>
     </PageWrapper>
   )
