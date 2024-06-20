865b011d432a47f0e0d0071cdc0c9f3cf8035ec2
Alex666000 <bogdanov777000@mail.ru>
2024-06-20 12:12:13 +0300

    temp-3

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -2,6 +2,7 @@ import { getBaseAppLayout, PageWrapper } from '@/shared/layouts'
 import { Text } from '@/shared/ui/text'
 import Avatar from '@/shared/ui/avatar/avatar'
 import { Button, Flex, useTranslation } from '@/shared'
+import Link from 'next/link'
 
 const Profile = () => {
   const { t } = useTranslation()
@@ -30,7 +31,7 @@ const Profile = () => {
               </Button>
             </Flex>
             <Flex className={`_Followers-block_ w-full max-w-[396px]`}>
-              <ul className={`flex w-full gap-[100px]`}>
+              <ul className={`_Followers-block-list_ flex w-full gap-[100px]`}>
                 <li className={`flex flex-col items-center`}>
                   <span>2 218</span> {t.pages.profile.following}
                 </li>
@@ -42,11 +43,23 @@ const Profile = () => {
                 </li>
               </ul>
             </Flex>
-            <p className={`_Profile-statistics-block_description_ pb-[53px] pt-[23px]`}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-              exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-            </p>
+            {/* todo: Text - aboutMe текст что с сервера должен прийти.. */}
+            <div
+              className={`_Profile-statistics-block_description_ w-full text-balance pb-[53px] pt-[23px]`}
+            >
+              <Text variant="regular_text_16">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                exercitation ullamco
+              </Text>
+              <Text
+                asComponent={`span`}
+                className={`pl-1.5 text-Primary-500 underline`}
+                variant={'regular_text_16'}
+              >
+                laboris nisi ut aliquip ex ea commodo consequat.
+              </Text>
+            </div>
           </div>
         </div>
         <div className={`profile-bottom-gallery_ grid`}></div>
