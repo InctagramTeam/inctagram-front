a2518ef78cd35ecf827b67e98abd7d468f2de711
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-06-23 12:01:44 +0300

    refactor request-item

--- src/feature/search-users/ui/request-item/request-item.tsx
+++ src/feature/search-users/ui/request-item/request-item.tsx
@@ -1,12 +1,23 @@
 import { RequestItemProps } from '@/feature/search-users/model'
 import { Flex, ReturnComponent, Text, UserAvatar } from '@/shared'
+import Link from 'next/link'
 
 export const RequestItem = ({ ava, name, nickname }: RequestItemProps): ReturnComponent => {
   return (
     <Flex gap={'12'}>
       <UserAvatar src={ava} userName={name} className={'h-[3rem] w-[3rem]'} />
       <div>
-        <Text className={'block underline'} variant={'bold_text_14'}>
+        <Text
+          asComponent={Link}
+          href={
+            {
+              // todo: данные с сервера для редиректа на страницу конкретного профиля
+              // `${AppRoutes.PROFILE}/${data?.id}`
+            }
+          }
+          className={'block underline'}
+          variant={'bold_text_14'}
+        >
           {nickname}
         </Text>
         <Text className={'block text-Light-900'} variant={'regular-text-14'}>
