18da67c77105eafb71f2b10f2a0f177569b3d14c
Alex666000 <bogdanov777000@mail.ru>
2024-06-26 09:16:51 +0300

    fix: typescript bug to sign-up form

--- src/shared/ui/app-link/app-link.tsx
+++ src/shared/ui/app-link/app-link.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx'
 import Link from 'next/link'
 import { ReactNode } from 'react'
+import { UrlObject } from "url";
 
 export type AppLinkVariant = 'primary' | 'red'
 
@@ -8,13 +9,15 @@ export type AppLinkProps = {
   variant?: AppLinkVariant
   children?: ReactNode
   activeLink?: string
+  href?: string | UrlObject
 } & Parameters<typeof Link>[0]
 
 /** Обёртка над дефолтным Link от Next.js */
 export function AppLink(props: AppLinkProps) {
-  const { className, variant = 'primary', children, activeLink, ...rest } = props
+  const { className, variant = 'primary', children, activeLink, href, ...rest } = props
   return (
     <Link
+      href={href}
       {...rest}
       className={clsx(
         className,
