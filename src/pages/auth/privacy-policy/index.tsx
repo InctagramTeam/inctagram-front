3f1ca6a1a0b9a83ec98dc54a57cc914e994fafc5
Anna-Bekreneva <anjabekreneva@gmail.com>
2024-07-05 17:07:11 +0300

    feat: add privacy-police page

--- src/pages/auth/privacy-policy/index.tsx
+++ src/pages/auth/privacy-policy/index.tsx
@@ -1,16 +1,19 @@
-import { getAuthLayout, useTranslation } from '@/shared'
-import { PageWrapper } from '@/widgets/page-wrapper'
+import { getAuthLayout, ReturnComponent, useResponsive, useTranslation } from '@/shared'
+import { PageWrapper } from '@/widgets'
+import { PrivacyPolice } from '@/feature'
 
-const PrivacyPolicyPage = () => {
+const PrivacyPolicyPage = (): ReturnComponent => {
   const { t } = useTranslation()
+  const { sm } = useResponsive()
 
   return (
     <PageWrapper
+      className={'max-w-[1268px] justify-start'}
       description={t.pages.privacyPolice.metaDescription}
-      paddingBlock={'72px'}
+      paddingBlock={sm ? '16px' : '24px'}
       title={t.pages.privacyPolice.metaTitle}
     >
-      <h1>PrivacyPolicy</h1>
+      <PrivacyPolice />
     </PageWrapper>
   )
 }
