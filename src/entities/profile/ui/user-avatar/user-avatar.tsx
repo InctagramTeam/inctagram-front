4ce7d06a88a6ac694f47aba3860d44bde4aeb254
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-09-18 12:56:14 +0300

    refactor: Added missing props to control the background and add an icon to center the avatar.

--- src/entities/profile/ui/user-avatar/user-avatar.tsx
+++ src/entities/profile/ui/user-avatar/user-avatar.tsx
@@ -8,12 +8,18 @@ type Props = {
   className?: string
   src?: string
   userName?: string
+  bgColor?: string
+  children?: React.ReactNode
 }
-export const UserAvatar = memo(({ className, src, userName }: Props): ReturnComponent => {
-  return (
-    <Avatar className={className}>
-      <AvatarImage alt={'user-avatar'} src={src} />
-      <AvatarFallback className={'bg-Light-900'}>{userName?.[0] || 'U'}</AvatarFallback>
-    </Avatar>
-  )
-})
+export const UserAvatar = memo(
+  ({ className, bgColor, src, userName, children }: Props): ReturnComponent => {
+    return (
+      <Avatar className={className}>
+        <AvatarImage alt={'user-avatar'} src={src} />
+        <AvatarFallback className={`${bgColor || 'bg-Light-900'}`}>
+          {children || userName?.[0] || 'U'}
+        </AvatarFallback>
+      </Avatar>
+    )
+  }
+)
