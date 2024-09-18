8350a0c85253f4faa9999a291d351c06e6516245
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-09-18 12:48:29 +0300

    refactor: Changed the icon color to currentColor so you can change it from the code.

--- src/shared/assets/icons/ImageOutlineIcon.tsx
+++ src/shared/assets/icons/ImageOutlineIcon.tsx
@@ -3,7 +3,7 @@ import { SVGProps, Ref, forwardRef, memo } from 'react'
 const ImageOutlineIcon = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={36} height={36} fill="none" ref={ref} {...props}>
     <path
-      fill="#fff"
+      fill="currentColor"
       d="M30 0H6a6 6 0 0 0-6 6v24a6 6 0 0 0 6 6h24a6 6 0 0 0 6-6V6a6 6 0 0 0-6-6ZM6 4h24a2 2 0 0 1 2 2v16.72l-6.4-5.46a5.54 5.54 0 0 0-7.04 0L4 29.4V6a2 2 0 0 1 2-2Zm24 28H7.12l14-11.68a1.56 1.56 0 0 1 1.86 0L32 28v2a2 2 0 0 1-2 2Z"
     />
   </svg>
