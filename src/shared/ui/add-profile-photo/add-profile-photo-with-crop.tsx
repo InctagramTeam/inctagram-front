76c665b0f91b07c224705471a40e0487fdcead79
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-09-24 18:37:22 +0300

    feat: Added a modal window when the avatar button is clicked.

--- src/shared/ui/add-profile-photo/add-profile-photo-with-crop.tsx
+++ src/shared/ui/add-profile-photo/add-profile-photo-with-crop.tsx
@@ -1,4 +1,4 @@
-import { Button, Modal } from '@/shared'
+import { Button, Modal, Text } from '@/shared'
 import { UserAvatar } from '@/entities/profile'
 import ImageOutlineIcon from '@/shared/assets/icons/ImageOutlineIcon'
 import * as React from 'react'
@@ -11,6 +11,8 @@ import { DeleteAvatarIcon } from '@/shared/assets/icons/DeleteIcon'
 
 export const AddProfilePhotoWithCrop = () => {
   const avatarUrl = useRef('')
+  const [modalUpdateAvatarOpen, setModalUpdateAvatarOpen] = useState(false)
+  const [modalDeleteAvatarOpen, setModalDeleteAvatarOpen] = useState(false)
 
   const { mutate: deleteAvatar, isPending: isPendingDeleteAvatar } = useMutation({
     mutationFn: async () => {
@@ -28,14 +30,15 @@ export const AddProfilePhotoWithCrop = () => {
     onError: (err: Error) => {},
   })
 
-  const [modalOpen, setModalOpen] = useState(false)
+  const deleteAvatarHandler = () => {
+    avatarUrl.current = ''
+    setModalDeleteAvatarOpen(false)
+    // deleteAvatar()
+  }
+
   const updateAvatarHandler = (imgSrc: string) => {
     avatarUrl.current = imgSrc
     // updatePhoto(imgSrc)
-  }
-
-  const deleteAvatarHandler = () => {
-    deleteAvatar()
   }
 
   return (
@@ -48,14 +51,55 @@ export const AddProfilePhotoWithCrop = () => {
           src={avatarUrl.current}
         />
         {avatarUrl.current && (
-          <button className={`absolute right-4 top-4`} onClick={deleteAvatarHandler}>
-            <DeleteAvatarIcon />
-          </button>
+          // <button className={`absolute right-4 top-4`} onClick={deleteAvatarHandler}>
+          //   <DeleteAvatarIcon />
+          // </button>
+          <Modal
+            open={modalDeleteAvatarOpen}
+            onOpenChange={isOpen => setModalDeleteAvatarOpen(isOpen)}
+          >
+            <Modal.Button asChild>
+              <button
+                className={`absolute right-3 top-3`}
+                onClick={() => setModalDeleteAvatarOpen(true)}
+              >
+                <DeleteAvatarIcon />
+              </button>
+            </Modal.Button>
+            <Modal.Content
+              title="Delete Photo"
+              classNameChildrenWrapper={'px-[24px]'}
+              classNameTitle={'text-H1-20'}
+              classNameTitleContainer={'h-[59px]'}
+              classNameContent={'!max-w-[438px]'}
+            >
+              <div className={'flex flex-col'}>
+                <Text className={'mb-[54px] mt-[30px]'} variant={'regular_text_16'}>
+                  Are you sure you want to delete the photo?
+                </Text>
+                <div className={'mb-[36px] flex justify-end gap-x-[24px]'}>
+                  <Button
+                    variant={'outline'}
+                    className={'h-[36px] w-[96px] !p-0'}
+                    onClick={deleteAvatarHandler}
+                  >
+                    Yes
+                  </Button>
+                  <Button
+                    className={'h-[36px] w-[96px]'}
+                    onClick={() => setModalDeleteAvatarOpen(false)}
+                  >
+                    No
+                  </Button>
+                </div>
+              </div>
+            </Modal.Content>
+          </Modal>
         )}
       </div>
-      <Modal open={modalOpen} onOpenChange={isOpen => setModalOpen(isOpen)}>
+      <Modal open={modalUpdateAvatarOpen} onOpenChange={isOpen => setModalUpdateAvatarOpen(isOpen)}>
         <Modal.Button asChild>
-          <Button variant={'outline'} onClick={() => setModalOpen(true)}>
+          <Button variant={'outline'} onClick={() => setModalUpdateAvatarOpen(true)}>
             Add a Profile Photo
           </Button>
         </Modal.Button>
@@ -66,7 +110,10 @@ export const AddProfilePhotoWithCrop = () => {
           classNameTitleContainer={'h-[59px]'}
           classNameContent={'!max-w-[492px]'}
         >
-          <ImageCropper updateAvatar={updateAvatarHandler} closeModal={() => setModalOpen(false)} />
+          <ImageCropper
+            updateAvatar={updateAvatarHandler}
+            closeModal={() => setModalUpdateAvatarOpen(false)}
+          />
         </Modal.Content>
       </Modal>
     </div>
