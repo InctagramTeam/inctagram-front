22fabea75e751f5bf2487900812f6964a83b21c3
mabar84 <mabar@tut.by>
2024-10-26 02:32:34 +0300

    fix: slider styles fixed

--- src/shared/ui/swiper-slider/swiper-slider.tsx
+++ src/shared/ui/swiper-slider/swiper-slider.tsx
@@ -1,11 +1,10 @@
 'use client'
 import { AppImage } from '@/shared/ui/app-image'
+import { Navigation, Pagination } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
 
-// import 'swiper/css/bundle'
-
-// import s from './swiper-slider.module.scss'
 import './swiper-slider.css'
+
 type Props = {
   images: string[]
 }
@@ -15,7 +14,13 @@ export const SwiperSlider = (props: Props) => {
 
   return (
     <div className={'swiper'}>
-      <Swiper navigation pagination={{ clickable: true }} slidesPerView={1} spaceBetween={50}>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        navigation
+        pagination={{ clickable: true }}
+        slidesPerView={1}
+        spaceBetween={50}
+      >
         {images.map((image, index) => (
           <SwiperSlide key={index}>
             <AppImage alt={`Slide ${index}`} src={image} />
