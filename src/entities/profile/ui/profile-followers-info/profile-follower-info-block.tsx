0bf77a1b23589ab8786e0c7f660d6a48224edb25
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-10-28 18:10:01 +0300

    feat: Added a layout for the ProfileInfoUserStats block.

--- src/entities/profile/ui/profile-followers-info/profile-follower-info-block.tsx
+++ src/entities/profile/ui/profile-followers-info/profile-follower-info-block.tsx
@@ -3,6 +3,7 @@
 import { User } from '@/entities/profile'
 import { FollowersInfoHeader } from '@/entities/profile/ui/followers-info-header/followers-info-header'
 import { ProfileInfoDescription } from '@/entities/profile/ui/profile-info-description/profile-info-description'
+import { ProfileInfoUserStats } from '@/entities/profile/ui/profile-info-user-stats/profile-info-user-stats'
 
 type Props = {
   userInfo?: User
@@ -12,6 +13,14 @@ export const ProfileFollowerInfoBlock = ({ userInfo }: Props) => {
   return (
     <div className={`_profile-followers-info_ ml-[36px] w-full max-w-[734px] pl-[36px]`}>
       <FollowersInfoHeader profile={userInfo?.profile} />
+      <ProfileInfoUserStats // TODO заполнить данными с бека, когда бэки добавят инфу в эндпоинт
+        // following={userInfo?.following || 0}
+        // followers={userInfo?.followers || 0}
+        // publication={userInfo?.publication || 0}
+        following={2218}
+        followers={2358}
+        publication={2764}
+      />
       <ProfileInfoDescription aboutMe={userInfo?.profile?.aboutMe} />
     </div>
   )
---
+++ src/entities/profile/ui/profile-info-user-stats/profile-info-user-stats.tsx
@@ -0,0 +1,26 @@
+import { Text } from '@/shared'
+
+type Props = {
+  following: number
+  followers: number
+  publication: number
+}
+
+export const ProfileInfoUserStats = ({ following, followers, publication }: Props) => {
+  return (
+    <div className={`flex items-center gap-x-[88px]`}>
+      <div className={`flex flex-col`}>
+        <Text variant={'bold_text_14'}>{following}</Text>
+        <Text variant={'regular-text-14'}>Following</Text>
+      </div>
+      <div className={`flex flex-col`}>
+        <Text variant={'bold_text_14'}>{followers}</Text>
+        <Text variant={'regular-text-14'}>Followers</Text>
+      </div>
+      <div className={`flex flex-col`}>
+        <Text variant={'bold_text_14'}>{publication}</Text>
+        <Text variant={'regular-text-14'}>Publications</Text>
+      </div>
+    </div>
+  )
+}
