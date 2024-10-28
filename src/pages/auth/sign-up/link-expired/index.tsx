6ef00106878c1c391b42d6b25eb6a035de96c912
stasfilippov <kron888999@gmail.com>
2024-10-28 18:00:22 +0300

    feat: sign-up/link-expired page

--- src/pages/auth/sign-up/link-expired/index.tsx
+++ src/pages/auth/sign-up/link-expired/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react'
 
+import { useRegistrationEmailResending } from '@/feature'
 import {
   AuthRoutes,
   Button,
@@ -15,16 +16,20 @@ import { TimeManagementIllustration } from '@/shared/assets/illustrations'
 import { PageWrapper } from '@/widgets/page-wrapper'
 import { useRouter } from 'next/router'
 
-const ConfirmEmailPage = (): ReturnComponent => {
+const LinkExpiredEmail = (): ReturnComponent => {
   const { xs } = useResponsive()
   const { t } = useTranslation()
+
+  const { mutate } = useRegistrationEmailResending()
 
   const classes = {
     button: cn('py-[6px] px-[24px]', !xs && 'mb-[32px]', xs && 'order-1'),
     illustration: cn('max-w-[474px] w-full h-[354px]', xs && 'h-[246px] mb-[42px]'),
   }
-
-  const router = useRouter()
+  //TODO - обговорить с беками откуда получить email
+  const resendLinkHandler = () => {
+    mutate(email)
+  }
 
   return (
     <PageWrapper
@@ -38,9 +43,7 @@ const ConfirmEmailPage = (): ReturnComponent => {
             asComponent={'button'}
             className={classes.button}
             fullWidth={xs}
-            onClick={() => {
-              router.replace(AuthRoutes.FORGOT_PASSWORD)
-            }}
+            onClick={resendLinkHandler}
           >
             {t.button.resendVerificationLink}
           </Button>
@@ -54,5 +57,5 @@ const ConfirmEmailPage = (): ReturnComponent => {
   )
 }
 
-ConfirmEmailPage.getLayout = getAuthLayout
-export default ConfirmEmailPage
+LinkExpiredEmail.getLayout = getAuthLayout
+export default LinkExpiredEmail
