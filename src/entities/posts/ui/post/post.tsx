156135038cf5b7ee414c7d39e4fc41c58a31fab7
mabar84 <mabar@tut.by>
2024-11-05 19:43:25 +0300

    fix: post converted to tailwind

--- src/entities/posts/ui/post/post.tsx
+++ src/entities/posts/ui/post/post.tsx
@@ -8,34 +8,49 @@ import { Button, useTranslation } from '@/shared'
 import defaultAvatar from '@/shared/assets/images/avatar.svg'
 import { AppImage } from '@/shared/ui/app-image'
 import { CollapsingText } from '@/shared/ui/collapsing-text/collapsing-text'
-import clsx from 'clsx'
-import { formatDistanceToNow } from 'date-fns/formatDistanceToNow'
+import { formatDistanceToNow } from 'date-fns'
 import { enUS, ru } from 'date-fns/locale'
+import Link from 'next/link'
 
 import 'swiper/swiper-bundle.css'
-
-import s from './post.module.scss'
 
 export const Post = (props: PostItem) => {
   const { description, postImages, createdAt } = props
 
   const { locale: localeFromUseTranslation } = useTranslation()
-
   const locale = localeFromUseTranslation === 'ru' ? ru : enUS
 
   const slides = postImages.map(el => el.url)
   const timeAgo = formatDistanceToNow(new Date(createdAt), { addSuffix: true, locale })
 
-  return (
-    <div className={clsx(s.post)}>
-      <PostImageSlider images={slides} />
+  const isSlides = slides.length > 1
 
-      <Button className={s.openProfile} variant={'text'}>
-        <AppImage alt={'post'} className={s.avatar} src={defaultAvatar} />
-        <h3>URLProfile</h3>
+  return (
+    <div className={'flex h-[391px] w-[234px] flex-col'}>
+      {isSlides ? (
+        <PostImageSlider images={slides} />
+      ) : (
+        <Button asComponent={Link} href={'/'}>
+          <AppImage alt={'post-image'} className={'h-full w-full object-cover'} src={slides[0]} />
+        </Button>
+      )}
+
+      <Button
+        className={'group my-3 flex h-auto w-full items-start justify-start p-0 text-left'}
+        style={{ justifyContent: 'start' }}
+        variant={'text'}
+      >
+        <AppImage
+          alt={'post'}
+          className={
+            'max-h-[36px] max-w-[36px] rounded-full bg-white transition-all duration-150 ease-in-out group-hover:bg-Primary-500 hover:bg-Primary-500'
+          }
+          src={defaultAvatar}
+        />
+        <h3 className={'text-lg font-semibold leading-6'}>URLProfile</h3>
       </Button>
 
-      <p className={s.time}>{timeAgo}</p>
+      <p className={'mb-1 text-sm font-normal leading-4 text-Light-900'}>{timeAgo}</p>
 
       <CollapsingText value={description} />
     </div>
