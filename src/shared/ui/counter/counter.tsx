29302fb9e8ed264bedf32a20811068bb70fed1dc
mabar84 <mabar@tut.by>
2024-11-05 16:34:54 +0300

    fix: counter converted to tailwind

--- src/shared/ui/counter/counter.tsx
+++ src/shared/ui/counter/counter.tsx
@@ -1,6 +1,4 @@
 'use client'
-import s from './counter.module.scss'
-
 type Props = {
   value?: number
 }
@@ -14,15 +12,16 @@ export const Counter = (props: Props) => {
   }
 
   return (
-    <div className={s.counter}>
+    <div
+      className={
+        'flex h-[48px] w-[203px] items-center rounded-md border border-Dark-300 bg-Dark-700 p-2'
+      }
+    >
       {arr.map((el, i) => (
-        <>
-          <div className={s.divider}></div>
-
-          <p className={s.value} key={i}>
-            {el}
-          </p>
-        </>
+        <div className={'flex items-center'} key={i}>
+          {i !== 0 && <div className={'h-[30px] w-[1px] bg-Dark-300'}></div>}
+          <p className={'w-[30px] text-center text-lg font-bold leading-[30px]'}>{el}</p>
+        </div>
       ))}
     </div>
   )
