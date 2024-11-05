a4a6b6914dd331dd1186f901f441b2dc8490139c
mabar84 <mabar@tut.by>
2024-11-05 19:46:25 +0300

    fix: CollapsingText converted to tailwind

--- src/shared/ui/collapsing-text/collapsing-text.tsx
+++ src/shared/ui/collapsing-text/collapsing-text.tsx
@@ -3,9 +3,6 @@
 import { useState } from 'react'
 
 import { useTranslation } from '@/shared'
-import clsx from 'clsx'
-
-import s from './collapsing-text.module.scss'
 
 type Props = {
   className?: string
@@ -16,15 +13,11 @@ type Props = {
 
 export const CollapsingText = (props: Props) => {
   const { value, className, minLength = 83, maxLength = 158 } = props
-
   const [collapsed, setCollapsed] = useState(true)
   const { t } = useTranslation()
 
   const isNeedCollapse = value.length > minLength
-
-  const handleToggle = () => {
-    setCollapsed(!collapsed)
-  }
+  const handleToggle = () => setCollapsed(!collapsed)
 
   const text = collapsed
     ? value.slice(0, minLength) + '…'
@@ -33,16 +26,16 @@ export const CollapsingText = (props: Props) => {
   const togglingText = collapsed ? t.posts.showMore : t.posts.hide
 
   return (
-    <div className={clsx(s.block, className)}>
+    <div className={`text-sm font-normal leading-6 ${className}`}>
       {isNeedCollapse ? (
         <>
-          <span> {text} </span>
-          <span className={s.toggler} onClick={handleToggle}>
+          <span>{text}</span>
+          <span className={'cursor-pointer text-Primary-500 underline'} onClick={handleToggle}>
             {togglingText}
           </span>
         </>
       ) : (
-        <span> {value} </span>
+        <span>{value}</span>
       )}
     </div>
   )
