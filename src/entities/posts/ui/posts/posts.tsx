c230c91234d2114d04ac6a19209a01b0505345bf
mabar84 <mabar@tut.by>
2024-11-05 19:50:14 +0300

    fix: posts converted to tailwind

--- src/entities/posts/ui/posts/posts.tsx
+++ src/entities/posts/ui/posts/posts.tsx
@@ -5,8 +5,6 @@ import { Post } from '@/entities/posts/ui/post'
 import { UsersCounter } from '@/entities/posts/ui/users-counter'
 import { Flex } from '@/shared'
 
-import s from './posts.module.scss'
-
 type Props = {
   posts?: Posts
 }
@@ -15,7 +13,7 @@ export const PostsPage = (props: Props) => {
   const { posts } = props
 
   return (
-    <div className={s.posts}>
+    <div className={'flex flex-col gap-9'}>
       <UsersCounter value={posts?.usersCount} />
 
       <Flex gap={'12'} items={'start'} justify={'start'} wrap={'wrap'}>
