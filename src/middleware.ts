11a8321e4ff3c93212c5351cf3c83d9609614e35
MrEvgeniy1989 <mr.evgeniy1989@yandex.ru>
2024-11-08 13:14:32 +0300

    refactor: Removed redundant checking in middleware.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,26 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-// Маршруты, доступные только авторизованным пользователям
-const protectedRoutes = ['/my-profile/:id/settings']
-
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('accessToken')?.value
+  // const token = request.cookies.get('accessToken')?.value
   const userId = request.cookies.get('userId')?.value
   const { pathname } = request.nextUrl
 
-  // Проверка авторизации: если защищённый маршрут и нет токена, перенаправить на страницу логина
-  if (protectedRoutes.some(route => pathname.startsWith(route)) && !token) {
-    const signInUrl = new URL('/auth/sign-in', request.url)
-
-    return NextResponse.redirect(signInUrl)
-  }
+  // // Проверка авторизации: если защищённый маршрут и нет токена, перенаправить на страницу логина
+  // if (pathname.startsWith('/my-profile') && pathname.includes('/settings') && !token) {
+  //   return NextResponse.redirect(new URL('/auth/sign-in', request.url))
+  // }
 
   // Логика проверки, что пользователь не может зайти в настройки чужого профиля
   if (pathname.includes('/settings')) {
-    const profileId = pathname.split('/')[2] // Получаем ID профиля из URL
+    const profileId = pathname.split('/')[2] // Извлекаем ID профиля из URL
 
     // Если userId не совпадает с profileId, перенаправляем на страницу профиля
     if (userId !== profileId) {
-      const profileUrl = new URL(`/my-profile/${profileId}`, request.url)
-
-      return NextResponse.redirect(profileUrl)
+      return NextResponse.redirect(new URL(`/my-profile/${profileId}`, request.url))
     }
   }
 
-  // Позволяем продолжить загрузку страницы, если нет проблем
+  // Продолжаем загрузку страницы, если все проверки пройдены
   return NextResponse.next()
 }
 
