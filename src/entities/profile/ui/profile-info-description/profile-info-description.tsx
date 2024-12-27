86da3750a1c0d454a2b6e4ec06418b4f94b6d75e
stasfilippov <kron888999@gmail.com>
2024-12-27 11:49:52 +0300

    feat: update profile-info-description.tsx

--- src/entities/profile/ui/profile-info-description/profile-info-description.tsx
+++ src/entities/profile/ui/profile-info-description/profile-info-description.tsx
@@ -1,22 +1,13 @@
 import { Text, useTranslation } from '@/shared'
 
 type Props = {
-  aboutMe?: string
+  aboutMe: string
 }
 
 export const ProfileInfoDescription = ({ aboutMe }: Props) => {
-  const { t } = useTranslation()
-
   return (
     <div className={`_description_ w-full text-balance pb-[53px] pt-[23px]`}>
-      <Text variant={'regular_text_16'}>{aboutMe ? aboutMe : 'Not information'}</Text>
-      {/*<Text*/}
-      {/*  asComponent={`a`}*/}
-      {/*  className={`pl-1.5 text-Primary-500 underline hover:cursor-pointer`}*/}
-      {/*  variant={'regular_text_16'}*/}
-      {/*>*/}
-      {/*  laboris nisi ut aliquip ex ea commodo consequat.*/}
-      {/*</Text>*/}
+      <Text variant={'regular_text_16'}>{aboutMe}</Text>
     </div>
   )
 }
