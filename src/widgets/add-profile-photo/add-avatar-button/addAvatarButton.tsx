61f85820550f2124027dbdc413bf1e9db06a7b46
Anna-Bekreneva <anjabekreneva@gmail.com>
2025-01-17 21:31:08 +0300

    merge

--- src/widgets/add-profile-photo/add-avatar-button/addAvatarButton.tsx
+++ src/widgets/add-profile-photo/add-avatar-button/addAvatarButton.tsx
@@ -1,15 +1,13 @@
 import * as React from 'react'
 import { useState } from 'react'
 
-import { Profile } from '@/entities/profile'
 import { Button, Modal, ModalClose, ModalContent, useTranslation } from '@/shared'
-import ImageCropper from '@/shared/ui/add-profile-photo/add-avatar-button/image-cropper/image-cropper'
+import ImageCropper from '@/widgets/add-profile-photo/add-avatar-button/image-cropper/image-cropper'
 
 type Props = {
-  profile: Profile
+  profileAvatar: null | string
 }
-
-export const AddAvatarButton = ({ profile }: Props) => {
+export const AddAvatarButton = ({ profileAvatar }: Props) => {
   const { t } = useTranslation()
   const [modalUpdateAvatarOpen, setModalUpdateAvatarOpen] = useState(false)
 
@@ -31,7 +29,10 @@ export const AddAvatarButton = ({ profile }: Props) => {
         classNameTitleContainer={'h-[59px]'}
         title={t.pages.profile.addProfilePhoto.title}
       >
-        <ImageCropper closeModal={() => setModalUpdateAvatarOpen(false)} profile={profile} />
+        <ImageCropper
+          closeModal={() => setModalUpdateAvatarOpen(false)}
+          profileAvatar={profileAvatar}
+        />
       </ModalContent>
     </Modal>
   )
